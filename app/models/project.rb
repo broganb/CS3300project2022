a508a5c2ec5b91474775d6fa608615a01f65997e
Brogan <broganbewley@gmail.com>
2022-11-28 21:27:38 +0000

    TestP rb change

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     # require title and description
-    validate :title, presence: true
-    validate :description, presence: true
+    validates :title, presence: true
+    validates :description, presence: true
 end
