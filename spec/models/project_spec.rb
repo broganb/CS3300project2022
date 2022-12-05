5e9432c53999dde3da1adde55edef7b5edc5a1fe
Brogan <broganbewley@gmail.com>
2022-12-05 06:05:08 +0000

    Iter 05

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,11 +2,17 @@ require "rails_helper"
 
 RSpec.describe Project, type: :model do
   context "validations tests" do
-    it "ensures the description is present" do
+    it "ensures the title is present" do
       project = Project.new(description: "Content of the description")
-      expect(project.valid?).to eq(true)
+      expect(project.valid?).to eq(false)
     end
-    
+
+    it "ensures the description is present" do
+      project = Project.new(title: "New title")
+      expect(project.valid?).to eq(false)
+    end
+
+
     it "should be able to save project" do
       project = Project.new(title: "Title", description: "Some description content goes here")
       expect(project.save).to eq(true)
